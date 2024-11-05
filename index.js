c359c1607169f6fb0cba2a25beab5376a6d16327
anuraaagz <mathuranurag1807@gmail.com>
2024-11-05 19:33:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const router = require("./routes/index");
+const dbConnect=require("./config/database");
+const cors = require("cors");
+app.use(cors());
+
+require('dotenv').config();
+const database = require("./config/database");
+database.dbConnect();
+
+
+const PORT = process.env.port || 4000 ;
+const coookieParser = require("cookie-parser");
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(coookieParser());  
+
+
+app.use("/api/v1" , router );
+
+app.listen(PORT , async ()=> {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+app.get("/", (req, res) => {
+    return res.json({
+        success: true, 
+        message: "Your server is running...",
+    })
+})
